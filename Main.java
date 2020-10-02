c789214efeb9c67d778d7be0213f48c03ba479e0
bazzl1202 <bazzl1202@wrdsb.ca>
2020-10-02 16:25:37 +0000

    Imported scanner

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author Liana Bazzarella
  */
 public class Main {
 
@@ -9,7 +10,10 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    
     
   }
 }
